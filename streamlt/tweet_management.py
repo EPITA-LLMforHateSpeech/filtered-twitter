81d7159e751ceb4d65051bf05fbb6066d01ccf4d
Muhammad Shaheryar <mshaheryar27@yahoo.com>
2024-07-23 09:03:10 +0200

    Updated tweet management

--- streamlt/tweet_management.py
+++ streamlt/tweet_management.py
@@ -30,6 +30,59 @@ class TweetManager:
         for user, user_data in users['usernames'].items():
             st.write(f"**{user_data['name']} ({user}):**")
             for tweet in user_data["tweets"]:
-                st.write(f"{tweet['text']}") 
+                st.write(f"{tweet['text']}(Likes: {tweet['like']}, Retweets: {tweet['retweet']}, Status: {tweet['safety_status']})")
+                if st.button(f'Like Tweet {tweet["id"]}', key=f'like_{tweet["id"]}'):
+                    self.like_tweet(username, tweet["id"])
+                    st.success('Tweet liked successfully')
+                if st.button(f'Retweet {tweet["id"]}', key=f'retweet_{tweet["id"]}'):
+                    self.retweet(username, tweet["id"])
+                    st.success('Retweeted successfully')
+                if st.button(f'Report Tweet {tweet["id"]}', key=f'report_{tweet["id"]}'):
+                    self.report_tweet(username, tweet["id"])
+                    st.success('Tweet reported successfully')
+                if st.button(f'Delete Tweet {tweet["id"]}', key=f'delete_{tweet["id"]}'):
+                    self.delete_tweet(user, tweet["id"])
+                    st.success('Tweet deleted successfully')
+                if st.button(f'Update Tweet {tweet["id"]}', key=f'update_{tweet["id"]}'):
+                    self.show_update_field(username, tweet["id"], tweet['text'])
+    
+    def show_update_field(self, username, tweet_id, current_text):
+        new_text = st.text_area(f'Update Tweet {tweet_id}', value=current_text, key=f'new_text_{tweet_id}')
+        if st.button(f'Submit Update {tweet_id}', key=f'submit_update_{tweet_id}'):
+            self.update_tweet(username, tweet_id, new_text)
+            st.success('Tweet updated successfully')
+ 
+    def like_tweet(self, username, tweet_id):
+        users = self.user_manager.load_users()
+        for user in users['usernames'].values():
+            for tweet in user["tweets"]:
+                if tweet["id"] == tweet_id:
+                    tweet["like"] += 1
+                    self.user_manager.save_users(users)
+                    return
+ 
+    def retweet(self, username, tweet_id):
+        users = self.user_manager.load_users()
+        tweet_to_retweet = None
+        for user in users['usernames'].values():
+            for tweet in user["tweets"]:
+                if tweet["id"] == tweet_id:
+                    tweet_to_retweet = tweet
+                    break
+            if tweet_to_retweet:
+                break
+ 
+        if tweet_to_retweet:
+            users['usernames'][username]["tweets"].append(tweet_to_retweet)
+            self.user_manager.save_users(users)
+ 
+    def report_tweet(self, username, tweet_id):
+        users = self.user_manager.load_users()
+        for user in users['usernames'].values():
+            for tweet in user["tweets"]:
+                if tweet["id"] == tweet_id:
+                    tweet["safety_status"] = "reported"
+                    self.user_manager.save_users(users)
+                    return
  
     
